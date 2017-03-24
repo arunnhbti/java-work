1eed3b4c88c6186ca38c0c0590635c96d272c960
arunnhbti <arunnhbti@gmail.com>
2017-03-24 09:10:56 +0530

    message

--- work-local-practices/src/test/java/com/arun/sample/SampleTest.java
+++ work-local-practices/src/test/java/com/arun/sample/SampleTest.java
@@ -1,26 +1,31 @@
 package com.arun.sample;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.example.mockito.MockitoExtension;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ Sample.class })
-public class SampleTest {
+@ExtendWith({ MockitoExtension.class })
+class SampleTest {
 
     @Mock
     TestA testA;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void init() {
     }
 
     @Test
-    public void testPrintName() throws Exception {
+    // void testPrintName(@Mock TestA testA) throws Exception {
+    void testPrintName() throws Exception {
         PowerMockito.mockStatic(TestA.class);
         PowerMockito.whenNew(TestA.class).withNoArguments().thenReturn(testA);
         Sample sample = new Sample();
