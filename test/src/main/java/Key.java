d8b42fbe1cc8118203a21c6e6a0d890986bc653c
Arun Kumar <arunnhbti@gmail.com>
2020-01-13 21:21:03 +0530

    Update Key.java

--- test/src/main/java/Key.java
+++ test/src/main/java/Key.java
@@ -1,14 +1,3 @@
-//  **********************************************************************
-//  Copyright (c) 2017 Ericsson Telekommunikation GmbH.
-//  All rights reserved.
-//  The Copyright to the computer program(s) herein is the property of
-//  Ericsson Telekommunikation GmbH.
-//  The program(s) may be used and/or copied with the written permission
-//  from Ericsson Telekommunikation GmbH or in accordance with the terms
-//  and conditions stipulated in the agreement/contract under which the
-//  program(s) have been supplied.
-//  **********************************************************************
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
